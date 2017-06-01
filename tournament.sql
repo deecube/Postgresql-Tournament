ed48ae4a4a9c00a30d31b423855c7fb22c6fc1d2
deecube <debdeep2007@gmail.com>
2017-06-01 23:42:36 +0530

    update sql queries to normalize the player_info table and the match_info

--- tournament.sql
+++ tournament.sql
@@ -1,8 +1,13 @@
--- Database schema for the Tournament project
+-- Table definitions for the tournament project.
+--
 
 DROP DATABASE IF EXISTS tournament;
+
 CREATE DATABASE tournament;
+
 \c tournament
+
+
 create table player_info (
 	id serial primary key, 
 	name text, 
@@ -10,8 +15,15 @@ create table player_info (
 	losses integer default 0, 
 	matches integer default 0
 	);
+
+
 create table match_info (
 	match_id serial primary key, 
 	winner integer references player_info(id), 
 	loser integer references player_info(id)
 	);
+
+
+ create view winners as select p.id,p.name, count(m.winner) as wins from player_info p left join match_info m on p.id=m.winner group by p.id;
+
+ create view noofmatches as select p.id, p.name, count(p.id) as matches from player_info p, match_info m where p.id=m.winner or p.id=m.loser group by p.id; 
