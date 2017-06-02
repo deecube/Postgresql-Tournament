9e2a8ebd796e4fd8dcf41a177b8996d59231d31a
deecube <debdeep2007@gmail.com>
2017-06-02 19:36:35 +0530

    create new VIEW standings and add ON CASCADE DELETE to the foreign keys in match_info table

--- tournament.sql
+++ tournament.sql
@@ -16,11 +16,23 @@ create table player_info (
 
 create table match_info (
 	match_id serial primary key, 
-	winner integer references player_info(id), 
-	loser integer references player_info(id)
+	winner integer references player_info(id) ON DELETE CASCADE, 
+	loser integer references player_info(id) ON DELETE CASCADE
 	);
 
 
- create view winners as select p.id,p.name, count(m.winner) as wins from player_info p left join match_info m on p.id=m.winner group by p.id;
+ CREATE VIEW winners AS 
+ SELECT p.id,p.name, COUNT(m.winner) AS wins 
+ FROM player_info p LEFT JOIN match_info m 
+ ON p.id=m.winner GROUP BY p.id;
 
- create view noofmatches as select p.id, p.name, count(p.id) as matches from player_info p, match_info m where p.id=m.winner or p.id=m.loser group by p.id; 
+ CREATE VIEW noofmatches AS 
+ SELECT p.id, p.name, COUNT(p.id) AS matches 
+ FROM player_info p, match_info m 
+ WHERE p.id=m.winner or p.id=m.loser 
+ GROUP BY p.id; 
+
+ CREATE VIEW standings AS 
+ SELECT n.id, n.name, w.wins, n.matches 
+ FROM noofmatches n JOIN winners w 
+ ON w.id = n.id ORDER BY wins DESC;
